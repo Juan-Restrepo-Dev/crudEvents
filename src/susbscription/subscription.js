cbde7a957d19620a33098e32ede39fe4263ef415
Delvidavid <david.990320@gmail.com>
2025-07-09 19:03:07 -0500

    Add files via upload

---
+++ src/susbscription/subscription.js
@@ -0,0 +1,57 @@
+window.addEventListener("DOMContentLoaded",()=>{
+
+const apiUrl = "http://localhost:3000/subscription";
+
+function Obtainsubscribed(){
+
+    fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => ShowSubscribed(data))
+    .catch((error)=>console.error("Error ",error))
+
+}
+
+function ShowSubscribed(subscription){
+
+    const subscribedlist = document.getElementById("container");
+    subscribedlist.innerHTML= "";
+
+    subscription.forEach((subscribed,index) =>{
+
+        const row = document.createElement("tr");
+
+        row.innerHTML= `
+            <td>${index + 1}</td>
+            <td>${subscribed.email}</td>
+            <td>${subscribed.date}</td>
+            <td><button type="button" class="btn btn-outline-primary" data-id=${subscribed.id}>Delete</button></td>        
+        `;
+
+    subscribedlist.appendChild(row);
+
+    });
+
+    document.querySelectorAll(".btn").forEach((button)=>{
+    button.addEventListener("click",(event)=>{
+        const id = event.target.getAttribute("data-id");
+        DeleteBook(id)
+    });
+
+    });
+
+};
+
+function DeleteBook(id) {
+    fetch(`${apiUrl}/${id}`, {
+    method: "DELETE",
+    })
+    .then(() => Obtainsubscribed())
+    .catch((error) => console.error("Error Deleting book:", error));
+}
+
+
+
+
+Obtainsubscribed();
+
+});
